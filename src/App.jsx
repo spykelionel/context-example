5208f3d9fd708cc0bb624465688f20c49749e57f
spykelion <ndilionel6@gmail.com>
2022-09-28 13:48:01 +0100

    add component tree

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,58 @@
-import React from 'react'
-import './App.css'
+import { useState, createContext, useContext } from "react";
 
+const UserContext = createContext();
 
-function App() {
+function ComponentA() {
+  const [user, setUser] = useState("Jane Doe");
 
   return (
-    <div className="App">
-     <h2>Context example</h2>
-    </div>
-  )
+    <UserContext.Provider value={user}>
+      <h1>{`Hello ${user}! from parent Component`}</h1>
+      <ComponentB />
+    </UserContext.Provider>
+  );
+}
+
+function ComponentB() {
+  return (
+    <>
+      <h1>Component 2</h1>
+      <ComponentC />
+    </>
+  );
+}
+
+function ComponentC() {
+  return (
+    <>
+      <h1>Component 3</h1>
+      <ComponentD />
+    </>
+  );
+}
+
+function ComponentD() {
+  return (
+    <>
+      <h1>Component 4</h1>
+      <ComponentE />
+    </>
+  );
+}
+
+function ComponentE() {
+  const user = useContext(UserContext);
+
+  return (
+    <>
+      <h1>Component 5</h1>
+      <h2>{`Hello ${user} from component E!`}</h2>
+    </>
+  );
+}
+
+function App (){
+  return <ComponentA></ComponentA>
 }
 
 export default App
