346c0a89a302dd667d2ef11963700cd283ab616a
spykelion <ndilionel6@gmail.com>
2022-09-28 10:33:50 +0100

    init app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+function App() {
+
+  return (
+    <div className="App">
+     <h2>Context example</h2>
+    </div>
+  )
+}
+
+export default App
